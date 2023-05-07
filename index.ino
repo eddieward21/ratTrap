e537c3fee215c1a2e33bfd33a3d6141237207dd4
Eddie Ward <eddie.musashi1@gmail.com>
2023-05-07 17:16:42 -0400

    Update index.ino

--- index.ino
+++ index.ino
@@ -8,35 +8,43 @@ const int servoPin = 7;
 Servo servo;
 
 // Flag to indicate whether the servo has already been activated
-bool servoActivated = false;
+bool trapActivated = false;
 
 void setup() {
+    Serial.begin(9600);
+
+    pinMode(sensorPin, INPUT); // Set the sensor pin as input
   // Attach the servo to its pin
   servo.attach(servoPin);
-      servo.write(180); // Set the servo to 90 degrees
-
+    servo.write(180);
+delay(1000);
   // Set up the serial monitor
-  Serial.begin(9600);
 }
 
 void loop() {
   // If the servo has already been activated, do nothing
-  if (servoActivated) {
+  if(trapActivated == true ) { 
     return;
   }
+
 
   // Read the input from the IR sensor
   int sensorValue = digitalRead(sensorPin);
 
   // If the sensor input is HIGH, activate the servo motor
-  if (sensorValue == HIGH) {
-    Serial.println("Sensor input received, activating servo.");
-    servo.write(0); // Set the servo to 90 degrees
-    delay(1000);     // Wait 1 second before resetting the servo
-    servo.detach();  // Set the servo back to 0 degrees
-    servoActivated = true; // Set the flag to indicate that the servo has been activated
-  }
+    if (sensorValue == HIGH) {
+      Serial.println("Sensor input received, activating servo.");
+            servo.write(0);    // Wait 1 second before resetting the servo
+            delay(1000);
+
+      servo.detach();
+ // Set the servo to 90 degrees
+      //servo.detach();  // Set the servo back to 0 degrees
+      trapActivated = true; // Set the flag to indicate that the servo has been activated
+      
+    }
+  
+
 
   // Wait for a short time before checking the sensor input again
-  delay(100);
 }
